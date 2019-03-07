2a6adb8d909f12f69b2356bb19acf2dd1b5ea1b0
Sungwook Yeom <krazyeom@gmail.com>
2019-03-07 17:04:26 +0900

    Change values for assert

--- function/python/brightics/function/classification/test/test_logistic_regression.py
+++ function/python/brightics/function/classification/test/test_logistic_regression.py
@@ -22,11 +22,11 @@ class TestLogisticRegression(unittest.TestCase):
                                                   res_train['model'],
                                                   prediction_col='prediction')
         table = res_predict['out_table'].values.tolist()
-        self.assertEqual(table[0], [5.1, 3.5, 1.4, 0.2, 'setosa', 'setosa', 0.8796816489561853, 0.1203075379065891, 1.0813137225508066e-05])
-        self.assertEqual(table[1], [4.9, 3.0, 1.4, 0.2, 'setosa', 'setosa', 0.7997063251281568, 0.2002632923353134, 3.0382536530016292e-05])
-        self.assertEqual(table[2], [4.7, 3.2, 1.3, 0.2, 'setosa', 'setosa', 0.853796794849413, 0.14617730202211324, 2.590312847381948e-05])
-        self.assertEqual(table[3], [4.6, 3.1, 1.5, 0.2, 'setosa', 'setosa', 0.8253831268363401, 0.17455893749671547, 5.793566694451843e-05])
-        self.assertEqual(table[4], [5.0, 3.6, 1.4, 0.2, 'setosa', 'setosa', 0.8973236276177116, 0.10266516737872604, 1.1205003562481428e-05])
+        self.assertEqual(table[0], [5.1, 3.5, 1.4, 0.2, 'setosa', 'setosa', 0.8780303050242847, 0.12195890005075813, 1.0794924957147882e-05])
+        self.assertEqual(table[1], [4.9, 3.0, 1.4, 0.2, 'setosa', 'setosa', 0.797058291879083, 0.20291141319673614, 3.0294924180971997e-05])
+        self.assertEqual(table[2], [4.7, 3.2, 1.3, 0.2, 'setosa', 'setosa', 0.8519976652686149, 0.14797647964561186, 2.585508577322853e-05])
+        self.assertEqual(table[3], [4.6, 3.1, 1.5, 0.2, 'setosa', 'setosa', 0.8234060190878218, 0.17653615914179305, 5.782177038521865e-05])
+        self.assertEqual(table[4], [5.0, 3.6, 1.4, 0.2, 'setosa', 'setosa', 0.8960349729152124, 0.10395383635092102, 1.119073386671762e-05])
 
 
 if __name__ == '__main__':
